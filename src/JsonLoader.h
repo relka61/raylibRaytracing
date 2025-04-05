358becb249ef9a3d1662055e112627e0246285b2
relka61 <157570128+relka61@users.noreply.github.com>
2025-04-05 14:18:06 +1100

    Update JsonLoader.h

--- src/JsonLoader.h
+++ src/JsonLoader.h
@@ -5,7 +5,7 @@
 #include <string>
 
 // Function declarations
-void loadMaterials(const std::string& filePath, int* materialType, Vector3* materialAlbedo, float* materialFuzz, float* materialRefractionIndex, std::string* materialTextures);
+void loadMaterials(const std::string& filePath, int* materialType, Vector3* materialAlbedo, Vector3* materialEmmisiveColor, float* materialFuzz, float* materialRefractionIndex, std::string* materialTextures);
 void loadSpheres(const std::string& filePath, Vector4* spheres, int* sphereMaterialIndices, int& spheresAmount);
 void loadQuads(const std::string& filePath, Vector3* quadOrigins, Vector3* quadEdgesU, Vector3* quadEdgesV, int* quadMaterialIndices, int& quadsAmount);
 
